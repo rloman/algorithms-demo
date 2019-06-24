94e79f5d56a696f6ae7419a8975b9dce6f9824d5
Raymond Loman <raymond@carpago.nl>
2019-06-24 13:51:43 +0200

    Add GGD (Grootste gemene deler) and KGV (Kleinste gemene veelvoud)

--- src/main/java/nl/hanze/algoritmiek/Algorithmiek.java
+++ src/main/java/nl/hanze/algoritmiek/Algorithmiek.java
@@ -4,12 +4,28 @@ public class Algorithmiek {
 
     public static int faculteit(int n) {
 
-        if(n==0) {
+        if (n == 0) {
             return 1;
+        } else {
+            return n * faculteit(n - 1);
         }
+    }
 
-        else {
-            return n * faculteit(n-1);
+
+    public static int ggd(int a, int b) {
+
+        if (a % b == 0) {
+            return b;
+        } else {
+            return ggd(b, a % b);
         }
+    }
+
+
+    public static int kgv(int a, int b) {
+
+        int result = (a*b)/ggd(a,b);
+
+        return result;
     }
 }
--- src/test/java/nl/hanze/algoritmiek/AlgorithmiekTest.java
+++ src/test/java/nl/hanze/algoritmiek/AlgorithmiekTest.java
@@ -11,4 +11,19 @@ public class AlgorithmiekTest {
         Assert.assertEquals(24, Algorithmiek.faculteit(4));
         Assert.assertEquals(120, Algorithmiek.faculteit(5));
     }
+
+    @Test
+    public void testGgd() {
+        Assert.assertEquals(9, Algorithmiek.ggd(27, 18));
+        Assert.assertEquals(6, Algorithmiek.ggd(18,6));
+        Assert.assertEquals(1, Algorithmiek.ggd(15,7));
+        Assert.assertEquals(10, Algorithmiek.ggd(100, 30));
+    }
+
+    @Test
+    public void testKgv() {
+        Assert.assertEquals(300, Algorithmiek.kgv(100, 30));
+        Assert.assertEquals(105, Algorithmiek.kgv(15,7));
+        Assert.assertEquals(15, Algorithmiek.kgv(15,5));
+    }
 }
